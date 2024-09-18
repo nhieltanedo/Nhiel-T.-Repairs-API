d4939528555316d019b9fa2f8e8be93415fbcafb
nhieltanedo <tanedonhielpatrick@gmail.com>
2024-09-19 00:54:43 +0800

    fixed cors 2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://nhiel-t-repairs-api.onrender.com'
+    'https://nhiel-t-repairs-api.onrender.com',
+    'https://nhiel-t-repairs.onrender.com'
 ]
 
 module.exports = allowedOrigins
