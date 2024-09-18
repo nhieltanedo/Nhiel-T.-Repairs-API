f4fabe792fa3ee3ca314ec867fef4a337a4b2482
nhieltanedo <tanedonhielpatrick@gmail.com>
2024-09-18 23:32:12 +0800

    ready to deploy

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com',
+     'http://localhost:3500'
+]
+
+module.exports = allowedOrigins
